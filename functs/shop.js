c257da45808e5b4a8c122b8eae40d3e6cb277a0d
TechTitan <160572406+dev-in-Church@users.noreply.github.com>
2024-09-30 17:21:37 +0300

    change shop.js code

--- functs/shop.js
+++ functs/shop.js
@@ -1,32 +1,82 @@
-//import { updateTrolleyQuantity } from "../data/trolley.js";
-import {
-  toggleMenu,
-  toggleSidebar,
-  owlCarouselFunc,
-  pageLoader,
-} from "./app.js";
+import { trolley, addToTrolley, pushToMemory } from "../data/trolley.js";
+import { products } from "../data/products.js";
+import { modifyCents } from "./utilities/currency.js";
+import { toggleMenu, toggleSidebar, pageLoader } from "./app.js";
+import { displayTrolleySummary } from "./checkout/trolleySmmary.js";
 import { renderHistoryHTML } from "../data/qrSummary.js";
+// import { removeQrList } from "../data/qrSummary.js";
 
 pageLoader();
+//sidebar javascript
 toggleSidebar();
-//toggleMenu();
 renderHistoryHTML();
-owlCarouselFunc();
-/*
-window.addEventListener("deviceorientation", (event) => {
-  console.log("Device Orientation:", event.alpha, event.beta, event.gamma);
+
+let productsHTML = "";
+
+products.forEach((product) => {
+  product.count == undefined
+    ? (product.count = "")
+    : (product.count = product.count);
+  productsHTML += `
+    <div class="item">
+      <img src="${product.image}" alt="" />
+      <h5>${product.name}</h5>
+      <div class="ppc">
+      <div class="price">$${modifyCents(product.price)}</div>
+      <div class="pc">${product.count}</div>
+      </div>
+      <button class="add-to-trolley-button button-primary js-add-to-trolley" data-product-id="${
+        product.id
+      }">
+      add to trolley
+      </button>
+    </div>
+  `;
 });
-*/
+
+document.querySelector(".js-productsList").innerHTML = productsHTML;
+
+//displayTrolleySummary();
 
 /*
-navigator.getBattery().then((battery) => {
-  console.log("Battery Level:", battery.level * 100 + "%");
-  console.log("Charging:", battery.charging ? "Yes" : "No");
+function updateTrolleyQuantity() {
+  trolley.forEach((trolleyItem) => {
+    //pushToMemory();
+  });
+}
+  */
+
+document.querySelectorAll(".js-add-to-trolley").forEach((button) => {
+  button.addEventListener("click", () => {
+    const productId = button.dataset.productId; //keep this code outside,, pass parameter'productId'
+
+    //addToTrolley();works also without the parameter...[parameters functs]
+    addToTrolley(productId); //pass the parameter when we call the function
+    //pushToMemory();
+    //updateTrolleyQuantity();
+    //displayTrolleySummary();
+  });
 });
 
-Notification.requestPermission().then((permission) => {
-  new Notification("Hello, World!");
-});
+/*
+let x = [1, 2, 3];
+let y = [...x];
+y.push(4);
+console.log(y);
 */
 
-console.log(new Date().getFullYear());
+/*log this and its functs*/
+
+// console.log(trolley);
+
+// document.querySelectorAll(".js-remove-qr").forEach((del) => {
+//   del.addEventListener("click", () => {
+//     // console.log("hello");
+
+//     const itemId = del.dataset.itemId;
+//     removeQrList(itemId);
+//     const container = document.querySelector(`.js-code-container-${itemId}`);
+//     container.remove();
+//     renderHistoryHTML();
+//   });
+// });
